735e4eadd47b7ec3ed839822ac3a1ab1707e05c4
Daniel E. Antonsen <danieleantonsen@gmail.com>
2023-11-06 16:45:10 +0100

    updated directory

---
+++ code/src/num_scheme.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+__author__ = 'Daniel Elisabethsønn Antonsen, UiT Institute of statistics and mathematics'
+
+# Importing libraries and modules
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+
+
+class heat_eq:
+
+    def __init__(self):
+        pass
+
+    def solve(self):
+        pass
+
+
